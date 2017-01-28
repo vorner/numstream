6889600a395c61940ed1b6a6c554da1adadeef00
Michal 'vorner' Vaner <vorner@vorner.cz>
2017-01-28 11:20:11 +0100

    Avoid unsafe code
    By using the byteorder crate. It's a bit of an overkill (we just want to
    dump the damn bytes, not reorder them), but it's definitely nicer code.
    Though it is a bit slower (maybe because of the use of slices?).

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+extern crate byteorder;
+
 use std::io::{self, Write};
+use byteorder::{ByteOrder, NativeEndian};
 
 const BLOCK_SIZE: u64 = 1024 * 16;
+const BLOCK_SIZE_BYTES: usize = BLOCK_SIZE as usize * 8;
 
 fn main() {
-    let mut block = [0u64; BLOCK_SIZE as usize];
+    let mut block = [0u8; BLOCK_SIZE_BYTES];
     let stdout_ll = io::stdout();
     let mut stdout = stdout_ll.lock();
     for i in 0.. {
         let offset = i * BLOCK_SIZE;
-        for (i, val) in block.iter_mut().enumerate() {
-            *val = offset + i as u64;
+        for i in 0..BLOCK_SIZE {
+            let index = i as usize;
+            NativeEndian::write_u64(&mut block[index * 8..(index + 1) * 8], i + offset);
         }
-        // There really should be a nicer way to get the bytes representation
-        let block_bytes: &[u8; BLOCK_SIZE as usize * 8] = unsafe { std::mem::transmute(&block) };
-        stdout.write_all(block_bytes).unwrap();
+        stdout.write_all(&block).unwrap();
     }
 }
